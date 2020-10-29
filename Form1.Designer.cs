b0986b2ae269cb74a4eaa0e8b824ad29cff35047
Gabriel Bomfim <73134861+tumbleweed-G@users.noreply.github.com>
2020-10-29 18:45:58 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TesteChart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(1, 1);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(441, 449);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 450);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace TesteChart1
+{
+    public partial class Form1 : Form
+    {
+        GraphPane graphPane;
+
+
+        private void DrawSineCurve()
+        {
+
+            PointPairList _pointPairList = new PointPairList();
+            //Gera o looping infinito da função seno, limitando ela dentro de um intervalo entre 0 n*2*PI
+            //Sendo n= (1, 2, 3, 4, ...), adicionando os pontos a um vetor imutável
+            for (int _angle = 0; _angle <= 360; _angle = _angle + 10)
+            {
+                double _x = _angle;
+                double _y = Math.Sin(Math.PI * _x / 100.0);
+                PointPair pointPair = new PointPair(_x, _y);
+
+                _pointPairList.Add(pointPair);
+            }
+            //Adiciona o pointPair ao gráfico, linha muito importante
+            LineItem _lineItem = graphPane.AddCurve("Sine Curve", _pointPairList, Color.Red, SymbolType.None);
+            
+            //Ajusta o gráfico para aparecer na escala correta para visualização rápida
+            zedGraphControl1.AxisChange();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Se comunica com o elemento gráfico.
+            graphPane = zedGraphControl1.GraphPane;
+
+            //Chama o Void Criado anteriormente para exutar
+            //o comando apenas no objeto quando adicionado o gráfico...
+            DrawSineCurve();
+        }
+
+
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
